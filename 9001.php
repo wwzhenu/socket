d2fcb040f4adeddd9f7ae3e44d121bd0238e7da8
wangwenzeng <272346623@qq.com>
2018-01-18 15:48:01 +0800

    save

--- 9001.php
+++ 9001.php
@@ -15,43 +15,35 @@ if (socket_bind($socket, $ip, $port) === FALSE)
 if (socket_listen($socket, 4) === FALSE)
     die('socket_listen fail');
 #客户端socket
-$client=NULL;
-echo 'create server success,waiting for client or debug '.PHP_EOL;
-$data=NULL;
+$client = NULL;
+echo 'create server success,waiting for client or debug ' . PHP_EOL;
+$data = NULL;
 # 服务器端socket
-$server=NULL;
-do{
-    $get = socket_accept($socket);
-    $data = socket_read($get, 10000000);
-    if ($data=='wanglovechu'){
-        echo 'here is client socket'.PHP_EOL;
-        $client=$get;
-        echo 'client connected: the key is '.$data.PHP_EOL;
-        do{
-            socket_recv($client,$getData,10000000,0);
-            if ($getData!==FALSE) {
-                echo 'receive return data :' . $getData . PHP_EOL;
-                if (empty($server))
-                    echo 'wating for debug ' . PHP_EOL;
-                else
-                    socket_write($server, $getData, strlen($getData));
+$server = NULL;
+$sockets=[];
+do {
+    socket_select($read, $write, $expect, null);
+    if (in_array($socket, $read)) {
+        echo 'a new connection ' . PHP_EOL;
+        $get = socket_accept($socket);
+        $data = socket_read($get, 10000000);
+        if ($data == 'wanglovechu') {
+            echo "a client socket" . PHP_EOL;
+            $client = $get;
+        } else {
+            echo 'a server socket' . PHP_EOL;
+            $server = $get;
+        }
+        $read[] = $get;
+        unset($read[array_search($socket, $read)]);
+    } else {
+        foreach ($read as $sock) {
+            echo 'a new message ' . PHP_EOL;
+            $data = socket_read($sock, 10000000000);
+            foreach ($sockets as $wk => $w) {
+                if ($w != $sock)
+                    socket_write($w, $data,strlen($data));
             }
-        }while(1);
-    }else{
-        $server=$get;
-        echo 'here is server socket'.PHP_EOL;
-        echo 'receive debug data :'.$data.PHP_EOL;
-        if (empty($client))
-            echo "wating for client".PHP_EOL;
-        else{
-            socket_write($client,$data);
-            do{
-                socket_recv($server,$getData,10000000,0);
-                if ($getData!==FALSE){
-                    echo 'receive return data :'.$getData.PHP_EOL;
-                    socket_write($client,$getData,strlen($getData));
-                }
-            }while(1);
         }
     }
-}while(TRUE);
+} while (1);
