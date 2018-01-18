1b70cbac8d77d7d1f72c42a9f2e1cced8e0b0b82
wangwenzeng <272346623@qq.com>
2018-01-18 17:01:52 +0800

    debug

--- 9001.php
+++ 9001.php
@@ -23,7 +23,7 @@ $server = NULL;
 $sockets=[];
 $read=[];
 do {
-    $read[] = $socket;
+    $read = [$socket];
     /*if (!empty($client))
         $read=array_merge($read,$sockets);
     $read=array_unique($read);*/
