3ab358766b3bc563e8d541884c9ce89cbd35ec75
wangwenzeng <272346623@qq.com>
2018-01-18 13:01:33 +0800

    xdebug

--- 9001.php
+++ 9001.php
@@ -5,3 +5,29 @@
  * Date: 2018/1/18
  * Time: 12:37
  */
+$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+if ($socket === FALSE)
+    die('socket_create fail');
+$ip = 'localhost';
+$port = 9001;
+if (socket_bind($socket, $ip, $port) === FALSE)
+    die('socket_bind fail');
+if (socket_listen($socket, 4) === FALSE)
+    die('socket_listen fail');
+$client=NULL;
+echo 'create server success,waiting for client or debug '.PHP_EOL;
+$data=NULL;
+do{
+    $get = socket_accept($socket);
+    $data = socket_read($get, 10000000);
+    if ($data=='wanglovechu'){
+        $client=$get;
+        echo 'client connected'.PHP_EOL;
+    }else{
+        echo 'receive debug data'.PHP_EOL;
+        if (empty($client))
+            echo "wating for client".PHP_EOL;
+        else
+            socket_write($client,$data);
+    }
+}while(TRUE);
--- client.php
+++ client.php
@@ -5,3 +5,13 @@
  * Date: 2018/1/18
  * Time: 12:50
  */
+$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+$client = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+socket_connect($socket, 'www.wanglovechu.com', 9002);
+socket_connect($client, 'localhost', 9001);
+$data='wanglovechu';
+socket_write($socket, $data, strlen($data));
+do{
+    socket_recv($socket, $data, 10000000, 0);
+    socket_write($client,$data,strlen($data));
+}while(1);
