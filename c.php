192ab56702315560058319fa1c79d2ec8b3c9106
wangwenzeng <272346623@qq.com>
2018-01-22 15:06:11 +0800

    debug

--- c.php
+++ c.php
@@ -5,3 +5,38 @@
  * Date: 2018/1/22
  * Time: 14:44
  */
+# 连接远程
+$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+if ($socket === FALSE)
+    die('socket_create fail');
+$ip="127.0.0.1";
+$port=9000;
+socket_connect($socket, $ip, $port);
+socket_write($socket,'wang',4);
+
+#连接PHPstorm
+$phpstorm = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+if ($phpstorm === FALSE)
+    die('socket_create fail');
+$ip="127.0.0.1";
+$port=9000;
+socket_connect($phpstorm, $ip, $port);
+
+$read[] = $socket;
+$read[] = $phpstorm;
+
+do{
+    socket_select($read,$w,$e,NULL);
+    foreach ($read as $s){
+        # 收到远程数据
+        if ($s==$socket){
+            $data=socket_read($s,10000000);
+            socket_write($phpstorm,$data,strlen($data));
+        }
+        # 收到PHPstorm数据
+        elseif ($s==$phpstorm){
+            $data=socket_read($s,10000000);
+            socket_write($socket,$data,strlen($data));
+        }
+    }
+}while(1);
--- s.php
+++ s.php
@@ -5,3 +5,48 @@
  * Date: 2018/1/22
  * Time: 14:33
  */
+# 服务器将数据发送至此接口
+$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+if ($socket === FALSE)
+    die('socket_create fail');
+$ip = '0.0.0.0';
+$port = 9001;
+if (socket_bind($socket, $ip, $port) === FALSE)
+    die('9001 socket_bind fail');
+if (socket_listen($socket, 4) === FALSE)
+    die('9001 socket_listen fail');
+
+$client=NULL;
+$server=NULL;
+
+$read[]=$socket;
+do{
+    socket_select($read,$w,$e,NULL);
+    foreach ($read as $s){
+        if ($s==$socket){
+            $get=socket_accept($s);
+            $data=socket_read($get,10000000);
+            # 收到客户端数据
+            if ($data=='wang'){
+                $client=$get;
+            }
+            # 收到chrome数据
+            else{
+                $server=$get;
+                if (isset($client))
+                    socket_write($client,$data,strlen($data));
+            }
+        }else if($s==$client){
+            $data=socket_read($s,10000000);
+            socket_write($server,$data,strlen($data));
+        }else if ($s==$server){
+            $data=socket_read($s,10000000);
+            socket_write($client,$data,strlen($data));
+        }
+    }
+    $read=[$socket];
+    if (isset($client))
+        $read[]=$client;
+    if (isset($server))
+        $read[]=$server;
+}while(1);
